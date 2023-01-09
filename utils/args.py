b5a38e7218d32006722bd0243e3e130aa6f5c381
x-stream <790559478@qq,com>
2023-01-09 21:10:30 +0800

    Support for changing tagging model

--- utils/args.py
+++ utils/args.py
@@ -17,6 +17,8 @@ def add_argument_base(arg_parser):
     arg_parser.add_argument('--seed', default=999, type=int, help='Random seed')
     arg_parser.add_argument('--device', type=int, default=-1, help='Use which device: -1 -> cpu ; the index of gpu o.w.')
     arg_parser.add_argument('--testing', action='store_true', help='training or evaluation mode')
+    arg_parser.add_argument('--model', default="baseline", help='Model for tagging')
+    
     #### Training Hyperparams ####
     arg_parser.add_argument('--batch_size', default=32, type=int, help='Batch size')
     arg_parser.add_argument('--lr', type=float, default=1e-3, help='learning rate')
