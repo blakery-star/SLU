60ce2baaba76103028fb9928b90a4bb149b75e96
x-stream <790559478@qq,com>
2023-01-11 20:53:38 +0800

    change the save path of model

--- scripts/slu_tagging.py
+++ scripts/slu_tagging.py
@@ -58,11 +58,13 @@ else:
 
 Example.word2vec.load_embeddings(TagModel.word_embed, Example.word_vocab, device=device)
 
+model_file_path = args.model+"_"+args.decode+"_"+args.train_data+"_"+args.encoder_cell+"_batch_size{}_lr{}_max_epoch{}_dropout{}_embed_size{}_hidden_size{}_num_layer{}_seed{}".format(args.batch_size,args.lr,args.max_epoch,args.dropout,args.embed_size,args.hidden_size,args.num_layer,args.seed)
+model_path=os.path.join(model_file_path,"model.bin")
+
 if args.testing:
-    check_point = torch.load(open(args.model+'.bin', 'rb'), map_location=device)
+    check_point = torch.load(open(model_path, 'rb'), map_location=device)
     TagModel.load_state_dict(check_point['TagModel'])
     print("Load saved TagModel from root path")
-
 
 def set_optimizer(model, args):
     params = [(n, p) for n, p in model.named_parameters() if p.requires_grad]
@@ -149,10 +151,11 @@ if not args.testing:
         print('Evaluation: \tEpoch: %d\tTime: %.4f\tDev acc: %.2f\tDev fscore(p/r/f): (%.2f/%.2f/%.2f)' % (i, time.time() - start_time, dev_acc, dev_fscore['precision'], dev_fscore['recall'], dev_fscore['fscore']))
         if dev_acc > best_result['dev_acc']:
             best_result['dev_loss'], best_result['dev_acc'], best_result['dev_f1'], best_result['iter'] = dev_loss, dev_acc, dev_fscore, i
+            if not os.path.exists(model_file_path):os.mkdir(model_file_path)
             torch.save({
                 'epoch': i, 'TagModel': TagModel.state_dict(),
                 'optim': optimizer.state_dict(),
-            }, open(args.model+'.bin', 'wb'))
+            }, open(model_path, 'wb'))
             print('NEW BEST TAGMODEL: \tEpoch: %d\tDev loss: %.4f\tDev acc: %.2f\tDev fscore(p/r/f): (%.2f/%.2f/%.2f)' % (i, dev_loss, dev_acc, dev_fscore['precision'], dev_fscore['recall'], dev_fscore['fscore']))
 
     print('FINAL BEST RESULT: \tEpoch: %d\tDev loss: %.4f\tDev acc: %.4f\tDev fscore(p/r/f): (%.4f/%.4f/%.4f)' % (best_result['iter'], best_result['dev_loss'], best_result['dev_acc'], best_result['dev_f1']['precision'], best_result['dev_f1']['recall'], best_result['dev_f1']['fscore']))
